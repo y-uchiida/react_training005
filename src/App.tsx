1f8c39a4e0415e2187179bcacedd0d0492d90977
yugo uchiida <y.uchiida@nirneege.com>
2022-09-13 23:51:03 +0900

    styling Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Sidebar } from './components/Sidebar'
+import { Main } from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <Sidebar />
+      <Main />
     </div>
   )
 }
