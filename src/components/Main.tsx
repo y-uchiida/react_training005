050540fcde3e219a7c4b214ef223ba11db7a4c7f
yugo uchiida <y.uchiida@nirneege.com>
2022-09-14 23:07:22 +0900

    styling Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,15 @@ import './Main.css'
 
 export const Main = () => {
 	return (
-		<div>Main</div>
+		<div className='app-main'>
+			<div className="app-main-note-edit">
+				<input type="text" value='new note' />
+				<textarea placeholder='input content here ...'></textarea>
+			</div>
+			<div className='app-main-note-preview'>
+				<h1 className='preview-title'>new note</h1>
+				<div className='markdown-preview'>input content here ...</div>
+			</div>
+		</div>
 	)
 }
